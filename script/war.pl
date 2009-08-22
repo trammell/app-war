5ec4b86a21fafea5ebbbb744dd1df9e3c39e5c28
John Trammell <jotr@voh.(none)>
2009-08-22 11:40:09 -0500

    fleshing out the product

---
+++ script/war.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Pod::Usage;
+use App::War;
+
+GetOptions(\%OPT,'file|f');
+
+my $war = App::War->new();
+
+#FIXME: get items
+#FIXME: run the war
+#FIXME: print the results
+
+
+
+=pod
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTIONS
+
+=head1 ARGUMENTS
+
+=cut
+
